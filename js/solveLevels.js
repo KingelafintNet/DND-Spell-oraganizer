2e6d2aa41acfbda75ede78b07c4b8c873a5e4b34
Charlie <finney.27@gmail.com>
2025-08-25 16:25:39 -0400

    main transfer

---
+++ js/solveLevels.js
@@ -0,0 +1,31 @@
+const classes = ["Artificer","Bard","Barbarian","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"]
+let levels = [];
+let casterLevel = 0;
+
+function hideUnusedLevels() {
+    let clas;
+    // checks each class and hides it if it has no levels
+    for (let i = 0; i < classes.length; i++) {
+        const element = classes[i];
+        clas = document.getElementById(element);
+        if (clas.value == 0) {
+            clas.parentElement.style.display = "none";
+            // Deletes the caster checkbox if class is unused
+            if (element == "Fighter" || element == "Rogue") {
+                document.getElementById(element+"Caster").parentElement.style.display = "none";
+            }
+        }
+        levels[i] = Number(clas.value);
+        // casterLevel += (i==1||i==3||i==4||i==10||i==12)?clas.value:(i=0||i==7||i==8)?clas.value/2:(i==5||i==9)?clas.value/3:0;
+    }
+    casterLevel += levels[1]+levels[3]+levels[4]+levels[10]+levels[12];
+    casterLevel += (levels[0]+levels[7]+levels[8])/2;
+    if (document.getElementById("FighterCaster").value) {
+        casterLevel += levels[5]/3;
+    }
+    if (document.getElementById("RogueCaster").value) {
+        casterLevel += levels[9]/3;
+    }
+    console.log(casterLevel);
+}
+
---
+++ js/userInterface.js
@@ -0,0 +1,4 @@
+function hideInput() {
+    let div = document.getElementById("classLevels");
+    div.style.right = -200;
+}

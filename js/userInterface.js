5bbafb348311c91e1591fe50d39cf05fc2c6746e
Charlie <finney.27@gmail.com>
2025-09-01 09:35:08 -0400

    added phone size testing function

--- js/userInterface.js
+++ js/userInterface.js
@@ -18,6 +18,31 @@ function hideInput() {
     }
 }
 
-function openAddSpell() {
-    
+function compileCharacter() {
+    document.getElementById("classLevels").style.display = "none";
+    document.getElementById("newSpell").style.display = "none";
+    let str = document.getElementById("Str").value;
+    let dex = document.getElementById("Dex").value;
+    let con = document.getElementById("Con").value;
+    let int = document.getElementById("Int").value;
+    let wis = document.getElementById("Wis").value;
+    let cha = document.getElementById("Cha").value;
+
+    let main = document.getElementById("main");
+    for (let i = 16; i > 0; i--) {
+        let add = document.createElement("div");
+        add.style.position = "absolute";
+        add.style.height = "30px";
+        add.style.width = i*30+"px";
+        add.style.backgroundColor = "rgba("+8*i+","+8*i+","+8*i+", 10)"
+        main.appendChild(add);
+    }
+    for (let i = 16; i > 0; i--) {
+        let add = document.createElement("div");
+        add.style.position = "absolute";
+        add.style.width = "30px";
+        add.style.height = i*30+"px";
+        add.style.backgroundColor = "rgba("+8*i+","+8*i+","+8*i+", 10)"
+        main.appendChild(add);
+    }
 }
